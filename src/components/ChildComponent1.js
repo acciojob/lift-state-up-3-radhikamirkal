92b0c96b6d3d05088bbd1d81f84ef17fcaea6ce4
radhikamirkal <radhikamirkal@gmail.com>
2026-03-13 19:36:12 +0530

    complete

---
+++ src/components/ChildComponent1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ChildComponent1 = ({ onSelect }) => {
+
+  return (
+    <div>
+      <h2>Child Component 1</h2>
+      <button onClick={() => onSelect("Option 1")}>
+        Option 1
+      </button>
+    </div>
+  );
+};
+
+export default ChildComponent1;
---
+++ src/components/ChildComponent2.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ChildComponent2 = ({ onSelect }) => {
+
+  return (
+    <div>
+      <h2>Child Component 2</h2>
+      <button onClick={() => onSelect("Option 2")}>
+        Option 2
+      </button>
+    </div>
+  );
+};
+
+export default ChildComponent2;
